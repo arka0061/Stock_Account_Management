63d6b3d9fe99391c3f152e8c7bbd269cf3f383b4
Arka <arka.spectacular@gmail.com>
2021-08-01 21:23:25 +0530

    [Arka]Added:Welcome Message

---
+++ Stock_Account_Management/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stock_Account_Management
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stock Account Management Program!");
+        }
+    }
+}
