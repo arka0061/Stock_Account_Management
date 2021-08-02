c3b590f1d235c270d9b8760dc0e3b21e2f200343
Arka <arka.spectacular@gmail.com>
2021-08-03 00:42:58 +0530

    [Arka]Added:Four Types of method for data processing

--- Stock_Account_Management/InventoryManagement/InventoryManager.cs
+++ Stock_Account_Management/InventoryManagement/InventoryManager.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Newtonsoft.Json;
-using Stock_Account_Management; 
+using Stock_Account_Management;
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
@@ -9,28 +9,67 @@ namespace Stock_Account_Management.InventoryManagement
 {
     public class InventoryManager
     {
-        public void Display(String filepath)
+
+        public void addAccount(String filepath)
+        {
+            string jsonData = File.ReadAllText(filepath);
+            InventoryModel jsonObjectArray = JsonConvert.DeserializeObject<InventoryModel>(jsonData);
+            List<Stock> stock = jsonObjectArray.StockList;
+            Stock obj = new Stock();
+            Console.WriteLine("Please Enter AccountName:");
+            obj.accountname = Console.ReadLine();
+            obj.stockName = "BMW";
+            obj.numberOfShares = 50;
+            obj.sharePrice = 50;
+            stock.Add(obj);
+            InventoryManager.WriteToFile(jsonObjectArray);
+            Console.WriteLine("Account Created!");
+            Console.WriteLine("------------------------------------------------------------------");
+            Console.WriteLine("Enter your Choice!");
+
+        }
+        public void valueOf(String filepath)
         {
             try
             {
                 if (File.Exists(filepath))
                 {
                     string jsonData = File.ReadAllText(filepath);
-                    double totalValue = 0;
+                    int counter = 0;
                     InventoryModel jsonObjectArray = JsonConvert.DeserializeObject<InventoryModel>(jsonData);
-                    Console.WriteLine("StockName" + "\t" + "Shares" + "\t" + "Price" + "\t" + "Value");
                     List<Stock> stock = jsonObjectArray.StockList;
+                    double totalValue = 0;
+                    Console.WriteLine("Enter Account name :");
+                    String Name = Console.ReadLine();
+                    Console.WriteLine("AccountName" + "\t" + "StockName" + "\t" + "Shares" + "\t" + "Price" + "\t" + "Value");
                     foreach (var item in stock)
                     {
-                        totalValue = totalValue + (item.numberOfShares * item.sharePrice);
-                        Console.WriteLine( "{0}" + "\t" + "\t"+"{1}" + "\t" + "{2}" + "\t" + "{3}", item.stockName, item.numberOfShares, item.sharePrice, item.numberOfShares*item.sharePrice);
+
+                        if (item.accountname == Name)
+                        {
+                            counter++;
+                            totalValue = totalValue + (item.numberOfShares * item.sharePrice);
+                            Console.WriteLine("{0}" + "\t" + "\t" + "{1}" + "\t" + "\t" + "{2}" + "\t" + "{3}" + "\t" + "{4}", item.accountname, item.stockName, item.numberOfShares, item.sharePrice, item.numberOfShares * item.sharePrice);
+                            Console.WriteLine("------------------------------------------------------------------");
+                            Console.WriteLine("Enter your Choice!");
+                        }
+                        else
+                        {
+                            if (counter == stock.Count)
+                                Console.WriteLine("Account Not Found!");
+                            Console.WriteLine("------------------------------------------------------------------");
+                            Console.WriteLine("Enter your Choice!");
+                        }
+
                     }
                     Console.WriteLine("Total Value of all Shares :" + totalValue);
-                    Console.WriteLine("-------------------------------------------------------------------------");
+                    Console.WriteLine("------------------------------------------------------------------");
+                    Console.WriteLine("Enter your Choice!");
                 }
                 else
                 {
                     Console.WriteLine("File Does not Exists");
+                    Console.WriteLine("------------------------------------------------------------------");
                     Console.WriteLine("Enter your Choice!");
                 }
             }
@@ -39,6 +78,194 @@ namespace Stock_Account_Management.InventoryManagement
                 throw new Exception(e.Message);
             }
         }
+
+        public void buy()
+        {
+            Stock obj = new Stock();
+            Program pro = new Program();
+            CompanyShares cmp = new CompanyShares();
+            string[] symbol = cmp.Symbol;
+            int[] amount = cmp.amount;
+            int[] pice = cmp.price;
+            string jsonData = File.ReadAllText(Program.Inventory_Json);
+            int counter = 0;
+            int counter2 = 0;
+            String SymbolName = "";
+            int amountOfShare = 0;
+            int i;
+            InventoryModel jsonObjectArray = JsonConvert.DeserializeObject<InventoryModel>(jsonData);
+            List<Stock> stock = jsonObjectArray.StockList;
+            Console.WriteLine("Enter Account name :");
+            String Name = Console.ReadLine();
+            Console.WriteLine("Enter Symbol :");
+            SymbolName = Console.ReadLine();
+            foreach (var item in stock.ToArray())
+            {
+                counter++;
+                if (item.accountname == Name && item.stockName == SymbolName)
+                {
+                    Console.WriteLine("Enter number of shares to ADD");
+                    amountOfShare = Convert.ToInt32(Console.ReadLine());
+                    item.numberOfShares = item.numberOfShares + amountOfShare;
+                    int TextIndex = Array.FindIndex(symbol, m => m == SymbolName);
+                    if (amountOfShare <= amount[TextIndex])
+                    {
+                        obj.numberOfShares = amountOfShare;
+                        obj.sharePrice = cmp.price[TextIndex];
+                        stock.Add(obj);
+                        Console.WriteLine("Shares Added!");
+                        Console.WriteLine("------------------------------------------------------------------");
+                        Console.WriteLine("Enter your Choice!");
+
+                        break;
+                    }
+                    else
+                    {
+
+                        Console.WriteLine("Shares not avaiable");
+                        Console.WriteLine("------------------------------------------------------------------");
+                        Console.WriteLine("Enter your Choice!");
+                        break;
+                    }
+                }
+                else
+                {
+
+                    if (counter == stock.Count)
+                    {
+                        Console.WriteLine("User First time buyng new stock :");
+                        Console.WriteLine("Enter Account name :");
+                        string name = Console.ReadLine();
+                        foreach (var items in stock)
+                        {
+                            counter2++;
+                            if (items.accountname == name)
+                            {
+                                obj.accountname = name;
+                                Console.WriteLine("Enter new Stock or Symbol :");
+                                obj.stockName = SymbolName;
+                                int TextIndex = Array.FindIndex(symbol, m => m == SymbolName);
+                                Console.WriteLine("Enter Amount of Share");
+                                amountOfShare = Convert.ToInt32(Console.ReadLine());
+                                if (amountOfShare <= amount[TextIndex])
+                                {
+                                    obj.numberOfShares = amountOfShare;
+                                    obj.sharePrice = cmp.price[TextIndex];
+                                    stock.Add(obj);
+                                    Console.WriteLine("Shares Added!");
+                                    Console.WriteLine("------------------------------------------------------------------");
+                                    Console.WriteLine("Enter your Choice!");
+
+                                    break;
+                                }
+                                else
+                                {
+
+                                    Console.WriteLine("Shares not avaiable");
+                                    Console.WriteLine("------------------------------------------------------------------");
+                                    Console.WriteLine("Enter your Choice!");
+
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                if (counter2 == stock.Count)
+                                    Console.WriteLine("Account Doesnt Exist!");
+                                Console.WriteLine("------------------------------------------------------------------");
+                                Console.WriteLine("Enter your Choice!");
+
+
+                            }
+                        }
+                    }
+                }
+            }
+            InventoryManager.WriteToFile(jsonObjectArray);
+        }
+
+        public void sell()
+        {
+            Stock obj = new Stock();
+            string jsonData = File.ReadAllText(Program.Inventory_Json);
+            int counter = 0;
+            String SymbolName = "";
+            int amountOfShare = 0;
+            int i;
+            InventoryModel jsonObjectArray = JsonConvert.DeserializeObject<InventoryModel>(jsonData);
+            List<Stock> stock = jsonObjectArray.StockList;
+            Console.WriteLine("Enter Account name :");
+            String Name = Console.ReadLine();
+            Console.WriteLine("Enter symbol of share to delete");
+            SymbolName = Console.ReadLine();
+            foreach (var item in stock)
+            {
+                counter++;
+                if (item.accountname == Name && item.stockName == SymbolName)
+                {
+                    Console.WriteLine("Enter number of shares to delete");
+                    amountOfShare = Convert.ToInt32(Console.ReadLine());
+                    item.numberOfShares = item.numberOfShares - amountOfShare;
+                    Console.WriteLine("Shares Sold!");
+                    Console.WriteLine("------------------------------------------------------------------");
+                    Console.WriteLine("Enter your Choice!");
+
+                    break;
+                }
+                else
+                {
+                    if (counter == stock.Count)
+                        Console.WriteLine("Account Doesnt Exist or Symbol Doesnt Exist!");
+                    Console.WriteLine("------------------------------------------------------------------");
+                    Console.WriteLine("Enter your Choice!");
+
+                }
+            }
+
+            InventoryManager.WriteToFile(jsonObjectArray);
+        }
+        public static void WriteToFile(InventoryModel inventory)
+        {
+            string jsonConvrsion = JsonConvert.SerializeObject(inventory);
+            System.IO.File.WriteAllText(@"E:\Bridglabz\Stock_Account_Management\Stock_Account_Management\InventoryManagement\InventoryOfStocks.json", jsonConvrsion);
+        }
+
+        public void printReport(String filepath)
+        {
+            try
+            {
+                if (File.Exists(filepath))
+                {
+                    string jsonData = File.ReadAllText(filepath);
+                    int counter = 0;
+                    InventoryModel jsonObjectArray = JsonConvert.DeserializeObject<InventoryModel>(jsonData);
+                    List<Stock> stock = jsonObjectArray.StockList;
+                    double totalValue = 0;                   
+                    Console.WriteLine("AccountName" + "\t" + "StockName" + "\t" + "Shares" + "\t" + "Price" + "\t" + "Value");
+                    foreach (var item in stock)
+                    {
+                        counter++;
+                        totalValue = totalValue + (item.numberOfShares * item.sharePrice);
+                        Console.WriteLine("{0}" + "\t" + "\t" + "{1}" + "\t" + "\t" + "{2}" + "\t" + "{3}" + "\t" + "{4}", item.accountname, item.stockName, item.numberOfShares, item.sharePrice, item.numberOfShares * item.sharePrice);
+                    }                  
+                    Console.WriteLine("Total Value of all Shares :" + totalValue);
+                    Console.WriteLine("------------------------------------------------------------------");
+                    Console.WriteLine("Enter your Choice!");
+                    }
+                    else
+                    {
+                    Console.WriteLine("File Does not Exists");
+                    Console.WriteLine("------------------------------------------------------------------");
+                    Console.WriteLine("Enter your Choice!");
+                    }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
     }
-    }
+}
+
+
 
--- Stock_Account_Management/Program.cs
+++ Stock_Account_Management/Program.cs
@@ -4,20 +4,41 @@ namespace Stock_Account_Management
 {
     class Program
     {
-       const String Inventory_Json = @"E:\Bridglabz\Stock_Account_Management\Stock_Account_Management\InventoryManagement\Inventory.json"; //@ define path
+       public const String Inventory_Json = @"E:\Bridglabz\Stock_Account_Management\Stock_Account_Management\InventoryManagement\InventoryOfStocks.json"; //@ define path
         static void Main(string[] args)
         {
             String Choice = "";
             InventoryManager main = new InventoryManager();         
-            Console.WriteLine("Enter 'disp' if u want to Display the items");
+            Console.WriteLine("Enter 'add' if u want to ADD an ACCOUNT");
+            Console.WriteLine("Enter 'buy' if u want to BUY some shares");
+            Console.WriteLine("Enter 'valueof' if u want to Display SHARES of an ACCOUNT");
+            Console.WriteLine("Enter 'printreport' if u want to Display SHARES of ALL ACCOUNTS");
+            Console.WriteLine("Enter stop to exit!");
             while (Choice != "stop")
             {
                 Choice = Console.ReadLine().ToLower();
                 switch (Choice)
                 {                   
-                   case "disp":
-                               main.Display(Inventory_Json);
-                                break;           
+                  
+                   case "add":
+                               main.addAccount(Inventory_Json);
+                               break;
+
+                    case "valueof":
+                               main.valueOf(Inventory_Json);
+                               break;
+
+                    case "buy":
+                               main.buy();
+                               break;
+
+                    case "sell":
+                               main.sell();
+                               break;
+
+                    case "printreport":
+                               main.printReport(Inventory_Json);
+                               break;
                 }
             }
 
