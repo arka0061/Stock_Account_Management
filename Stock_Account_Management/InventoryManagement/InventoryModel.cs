0cd038d14647a71c075f7a0bcb48597e0cb1dc76
Arka <arka.spectacular@gmail.com>
2021-08-02 13:41:25 +0530

    [Arka]Added:Displays ValueofStock and TotalStockValue

---
+++ Stock_Account_Management/InventoryManagement/InventoryModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stock_Account_Management.InventoryManagement
+{
+    public class InventoryModel
+    {
+        public List<Stock> StockList { get; set; }
+    }
+        
+}
---
+++ Stock_Account_Management/InventoryManagement/Stock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stock_Account_Management.InventoryManagement
+{
+    public class Stock
+    {
+        public String stockName { get; set; }
+        public int numberOfShares { get; set; }
+        public double sharePrice { get; set; }
+    }
+}
+
